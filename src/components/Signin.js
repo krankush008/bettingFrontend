c812ae527d26bac362b3a77448e4dbda1a2cf89f
Ankush Kumar <krankush008@gmail.com>
2023-10-01 22:38:03 -0700

    required field added and url assigned to variable for environment variables settings

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './Signin.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Signin.css';
 import axios from 'axios';
 
 function Signin(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
+  const backendSigninURl = 'http://localhost:8384/signin';
+
   const paragraphStyle = {
     color: 'red',
   };
 
-  const verifyInfo=async()=>{
-  
-    const response = await axios.get('http://localhost:8384/signin', {
+  const verifyInfo = async () => {
+    const emailInput = document.getElementById("email"); // Get the email input field
+    const passwordInput = document.getElementById("password"); // Get the password input field
+
+    // Check if email and password are empty
+    if (!email || !password) {
+      // Apply the red border to the empty fields
+      if (!email) {
+        emailInput.style.borderColor = "red";
+      }
+      if (!password) {
+        passwordInput.style.borderColor = "red";
+      }
+      // Display an error message to the user
+      setErrorMsg("Please fill in all required fields.");
+      return; // Prevent form submission
+    }
+
+    const response = await axios.get(backendSigninURl, {
       params: {
         email: email,
         password: password,
@@ -27,23 +45,25 @@ function Signin(props) {
         setErrorMsg(error.response.data.message);
     });
     
+    // Reset border colors after form submission
+    emailInput.style.borderColor = "";
+    passwordInput.style.borderColor = "";
   };
 
   return (
-
     <div className="wrapper bg-dark d-flex align-items-center justify-content-center w-100">
-      <div className="signup">
+      <div className="signin">
         <h2 className="mb-3">Signin Form</h2>
-          <div className="form-group mb-2">
-              <label htmlFor="email" className="form-label">Email Address</label>
-              <input type="email" className="form-control" onChange={(event) => setEmail(event.target.value)}></input>
-          </div>
-          <div className="form-group mb-2">
-              <label htmlFor="password" className="form-label">Password</label>
-              <input type="password" className="form-control"  onChange={(event) => setPassword(event.target.value)}></input>
-          </div>
-          <p style={paragraphStyle}>{errorMsg}</p>
-          <button type="submit" className="btn btn-success block mt-2" onClick={verifyInfo}>SIGN In</button>
+        <div className="form-group mb-2">
+          <label htmlFor="email" className="form-label">Email Address</label>
+          <input type="email" id="email" className="form-control" onChange={(event) => setEmail(event.target.value)} required ></input>
+        </div>
+        <div className="form-group mb-2">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" id="password" className="form-control" onChange={(event) => setPassword(event.target.value)} required ></input>
+        </div>
+        <p style={paragraphStyle}>{errorMsg}</p>
+        <button type="button" className="btn btn-success block mt-2" onClick={verifyInfo}>SIGN In</button>
       </div>
     </div>
   )
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './Signup.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Signup.css';
 import axios from 'axios';
 
 function Signup(props) {
@@ -8,52 +8,77 @@ function Signup(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
+  const backendSignupURl = 'http://localhost:8384/signup';
+
   const paragraphStyle = {
     color: 'red',
   };
 
   const postInfo = async () => {
-    
-      const response = await axios.post('http://localhost:8384/signup', {
-        method: 'POST',
-        headers: {
-          "Content-Type": 'application/json'
-        },
-        body: JSON.stringify({
-          username: username,
-          email: email,
-          password: password
-        })
+    const usernameInput = document.getElementById("username"); // Get the username input field
+    const emailInput = document.getElementById("email"); // Get the email input field
+    const passwordInput = document.getElementById("password"); // Get the password input field
+
+    // Check if any of the fields are empty
+    if (!username || !email || !password) {
+      // Apply the red border to the empty fields
+      if (!username) {
+        usernameInput.style.borderColor = "red";
+      }
+      if (!email) {
+        emailInput.style.borderColor = "red";
+      }
+      if (!password) {
+        passwordInput.style.borderColor = "red";
+      }
+      // Display an error message to the user
+      setErrorMsg("Please fill in all required fields.");
+      return; // Prevent form submission
+    }
+
+    const response = await axios.post(backendSignupURl, {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password
       })
-      .then((response) => {
-        props.updateVariables(true, false, false, false, false);
-        props.updateUserCreds(email, password)
+    })
+    .then((response) => {
+      props.updateVariables(true, false, false, false, false);
+      props.updateUserCreds(email, password)
     })
     .catch((error) => {
-         setErrorMsg(error.response.data.message);   
+      setErrorMsg(error.response.data.message);   
     });
-  
+
+    // Reset border colors after form submission
+    usernameInput.style.borderColor = "";
+    emailInput.style.borderColor = "";
+    passwordInput.style.borderColor = "";
   }  
 
   return (
-
     <div className="wrapper bg-dark d-flex align-items-center justify-content-center w-100">
       <div className="signup">
         <h2 className="mb-3">Signup Form</h2>
         <div className="form-group mb-2">
-              <label htmlFor="username" className="form-label">Username</label>
-              <input type="username" className="form-control"  onChange={(event) => setUsername(event.target.value)}></input>
-          </div>
-          <div className="form-group mb-2">
-              <label htmlFor="email" className="form-label">Email Address</label>
-              <input type="email" className="form-control" onChange={(event) => setEmail(event.target.value)}></input>
-          </div>
-          <div className="form-group mb-2">
-              <label htmlFor="password" className="form-label">Password</label>
-              <input type="password" className="form-control"  onChange={(event) => setPassword(event.target.value)}></input>
-          </div>
-          <p style={paragraphStyle}>{errorMsg}</p>
-          <button type="submit" className="btn btn-success block mt-2" onClick={postInfo}>SIGN UP</button>
+          <label htmlFor="username" className="form-label">Username</label>
+          <input type="text" id="username" className="form-control" onChange={(event) => setUsername(event.target.value)} required ></input>
+        </div>
+        <div className="form-group mb-2">
+          <label htmlFor="email" className="form-label">Email Address</label>
+          <input type="email" id="email" className="form-control" onChange={(event) => setEmail(event.target.value)} required ></input>
+        </div>
+        <div className="form-group mb-2">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" id="password" className="form-control" onChange={(event) => setPassword(event.target.value)} required ></input>
+        </div>
+        <p style={paragraphStyle}>{errorMsg}</p>
+        <button type="button" className="btn btn-success block mt-2" onClick={postInfo}>SIGN UP</button>
       </div>
     </div>
   )
